724a2f33116e7b20936563aa62ac9dd80f90eba8
Leonard Hansing <lhansing92@gmail.com>
2025-06-05 23:48:54 +0200

    some changes

--- packages/server/tsup.config.ts
+++ packages/server/tsup.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  target: "node18",
+  target: "node16",
   format: ["cjs"],
+  entry: ["src/index.ts"],
   splitting: false,
   sourcemap: false,
   clean: true,
   dts: false,
   minify: true,
-  noExternal: ["@nova-trials/shared"],
 });
--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   target: "es2020",
   format: ["cjs", "esm"],
+  entry: ["src/index.ts"],
   splitting: false,
   sourcemap: false,
   clean: true,
